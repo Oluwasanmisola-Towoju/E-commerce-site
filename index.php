d719ae825006c967d6dadef7e2ec8e134dbe9b06
hashtag._.01 <oluwasanmisolatowoju@gmail.com>
2025-11-01 00:55:00 +0100

    Created and designed website- body features

--- index.php
+++ index.php
@@ -33,7 +33,115 @@
         </nav>
 
         <div>
-            <img class="my_cover" src="cover.png" alt="dim(6912 x 3456)">
+            <img class="my_cover" src="cover.jpeg" alt="dim(6912 x 3456)">
+        </div>
+    <div>
+        <h3 class="p_title">
+            Products
+        </h3>
+    </div>
+
+        <div class="my_card">
+            <div class="card">
+                    <img class="p_image" src="watch.jpeg">
+                        <h4>
+                            Watch
+                        </h4>
+                            <p>
+                                Watch description
+                            </p>
+                            <p>
+                                Price: $50
+                            </p>
+                               <a href="#">Buy now</a>
+            </div>
+            <div class="card">
+                    <img class="p_image" src="phone.jpeg">
+                        <h4>
+                            Phone
+                        </h4>
+                            <p>
+                                Phone description
+                            </p>
+                            <p>
+                                Price: $50
+                            </p>
+                                <a href="#">Buy now</a>
+            </div>
+            <div class="card">
+                    <img class="p_image" src="shoe.jpeg">
+                        <h4>
+                            Shoe
+                        </h4>
+                            <p>
+                                Shoe description
+                            </p>
+                            <p>
+                                Price: $50
+                            </p>
+                               <a href="#">Buy now</a>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="footer_title">
+                <h3>My Ecommerce Site</h3>
+            </div>
+                <div class="footer_content">
+                </div>
+                    <div>
+                        <h4>Services</h4>
+                        <p>
+                            <a href="#">Web</a>
+                        </p>
+                        <p>
+                            <a href="#">App</a>
+                        </p>
+                        <p>
+                            <a href="#">Digital</a>
+                        </p>
+                    </div>
+                    <div>
+                        <h4>Social links</h4>
+                        <p>
+                            <a href="#">Facebook</a>
+                        </p>
+                        <p>
+                            <a href="#">X</a>
+                        </p>
+                        <p>
+                            <a href="#">Instagram</a>
+                        </p>
+                    </div>
+                    <div>
+                        <h4>Quick links</h4>
+                        <p>
+                            <a href="#">Home</a>
+                        </p>
+                        <p>
+                            <a href="#">Products</a>
+                        </p>
+                        <p>
+                            <a href="#">Contact</a>
+                        </p>
+                        <p>
+                            <a href="#">Register</a>
+                        </p>
+                        <p>
+                            <a href="#">Login</a>
+                        </p>
+                    </div>
+                    <div>
+                        <h4>Location</h4>
+                        <p>
+                            Western Union Street
+                        </p>
+                        <p>
+                            Email: myecomm@gmail.com
+                        </p>
+                        <p>
+                            Phone: 09019648094
+                        </p>
+                    </div>
         </div>
     </body>
 </html>
