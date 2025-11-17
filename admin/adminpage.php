14d67f207259e31515b278727cd7efbb4a67004c
hashtag._.01 <oluwasanmisolatowoju@gmail.com>
2025-11-17 17:49:32 +0100

    Customized the dashboard page of the admin panel

--- admin/adminpage.php
+++ admin/adminpage.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 
+$conn = mysqli_connect("localhost", "root", "", "php_e-comm");
+
+
 if(!isset($_SESSION['user_email'])){
     header("location:../home/login.php");
 }
@@ -8,6 +11,23 @@ if(!isset($_SESSION['user_email'])){
 elseif($_SESSION['usertype']=="user"){
     header("location:../home/login.php");
 }
+
+$user_sql = "SELECT * from user";
+$u_result = mysqli_query($conn,$user_sql);
+$total_user = mysqli_num_rows($u_result);
+
+$product_sql = "SELECT * from products";
+$u_result = mysqli_query($conn,$product_sql);
+$total_product = mysqli_num_rows($u_result);
+
+$order_sql = "SELECT * from orders";
+$u_result = mysqli_query($conn,$order_sql);
+$total_order = mysqli_num_rows($u_result);
+
+$status = 'Delivered';
+$deliver_sql = "SELECT * from orders Where status='$status' ";
+$u_result = mysqli_query($conn,$deliver_sql);
+$total_delivery = mysqli_num_rows($u_result);
 ?>
 
 <!DOCTYPE html>
@@ -46,8 +66,28 @@ elseif($_SESSION['usertype']=="user"){
                     <a href="../logout.php">Logout</a>
                 </div>
                 <div class="info">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis aperiam dolorem distinctio tempore omnis, excepturi ex vitae quam quis voluptates accusantium dolorum in alias obcaecati assumenda laudantium nihil pariatur vero.
-                    </p>
+                   <div class="card">
+                    <div class="my_card">
+                        <h3>Total Users</h3>
+                        <hr>
+                        <p><?php echo $total_user ?></p>
+                    </div>
+                    <div class="my_card">
+                        <h3>Total Products</h3>
+                        <hr>
+                        <p><?php echo $total_product ?></p>
+                    </div>
+                    <div class="my_card">
+                        <h3>Total Orders</h3>
+                        <hr>
+                        <p><?php echo $total_order ?></p>
+                    </div>
+                    <div class="my_card">
+                        <h3>Total Delivered</h3>
+                        <hr>
+                        <p><?php echo $total_delivery ?></p>
+                    </div>
+                   </div>
                 </div>
             </div>
         </div>
